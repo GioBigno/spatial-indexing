b3ef9c7747245782504ea822189d659a04aa1503
giorgio bignotti <giorgio.bigno@gmail.com>
2024-05-08 23:45:03 +0200

    improvemnts

--- kd-tree/kd-tree.hpp
+++ kd-tree/kd-tree.hpp
@@ -1,24 +1,69 @@
 #include <iostream>
 #include <vector>
+#include <limits>
+#include <cmath>
 #include <algorithm>
 
 namespace bigno{
 
-struct Point{
+class Point{
 
+	public:
 		Point(){
-			coordinates = {};
+			this->coordinates = {};
 		}
 
-		Point(std::vector<double>& v){
-			coordinates = v;
+		Point(std::vector<double>& v, void* data = nullptr){
+			this->coordinates = v;
+			this->setData(data);
 		}
 
 		std::size_t getDimensions() const {
-			return coordinates.size();
+			return this->coordinates.size();
 		}
 
+		double getCoordinate(const std::size_t dimension) const {
+			return this->coordinates[dimension];
+		}
+
+		void* getData() const {
+			return this->data;
+		}
+
+		std::string getDataString() const {
+			return this->data == nullptr ? "" : *(static_cast<std::string*>(this->getData()));
+		}
+
+		void setData(void* data){
+			this->data = data;
+		}
+
+		int cmp(const Point target, const std::size_t dimension) const {
+			double ret = this->coordinates[dimension] - target.coordinates[dimension];
+
+			if(fabs(ret) < std::numeric_limits<double>::epsilon()){
+				return 0;
+			}else{
+				return ret < 0 ? -1 : 1;
+			}
+		}
+
+		std::string toString(){
+			std::string ret = this->getDataString();
+			ret += "(";
+			for(std::size_t i=0; i<this->coordinates.size(); i++){
+				ret += std::to_string(this->coordinates[i]);
+				if(i != this->coordinates.size()-1)
+					ret += ", ";
+			}
+			ret += ")";
+			return ret;
+		}
+
+	private:
+
 		std::vector<double> coordinates;
+		void* data = nullptr;
 };
 
 class KdTree{
@@ -28,6 +73,42 @@ class KdTree{
 		KdTree(const std::size_t dimensions, std::vector<Point>& points){
 			this->dimensions = dimensions;
 			insert(this->root, points.begin(), points.end(), 0);
+		}
+		
+		std::vector<Point> query_neighbors(const Point start, const std::size_t numNeighbors) const {
+
+			if(this->root == nullptr){
+				return {};
+			}
+
+			Node* targetLeaf = this->root;
+			bool finished = false;
+
+			while(targetLeaf != nullptr && !finished){
+
+				std::cout<<"current target: "<<targetLeaf->point.getDataString()<<std::endl;
+
+				int cmp = start.cmp(targetLeaf->point, targetLeaf->dimension);
+
+				if(cmp < 0){
+					if(targetLeaf->left != nullptr){
+						targetLeaf = targetLeaf->left;
+					}else{
+						finished = true;
+					}
+				}else if(cmp > 0){
+					if(targetLeaf->right != nullptr){
+						targetLeaf = targetLeaf->right;
+					}else{
+						finished = true;
+					}
+				}
+			}
+
+			std::cout<<"targetLeaf: "<<targetLeaf->point.toString()<<std::endl;
+			
+			//temp
+			return {};
 		}
 
 	private:
@@ -42,9 +123,14 @@ class KdTree{
 				this->point = point;
 				this->dimension = dimension;
 			}
+
+			bool isLeaf(){
+				return this->left == nullptr && this->right == nullptr;
+			}
+
 		};
 
-		void insert(Node* node, std::vector<Point>::iterator begin, std::vector<Point>::iterator end, const std::size_t dimension){
+		void insert(Node*& node, std::vector<Point>::iterator begin, std::vector<Point>::iterator end, const std::size_t dimension){
 			
 			const std::size_t size = end-begin;
 
@@ -53,14 +139,19 @@ class KdTree{
 			}
 
 			std::sort(begin, end, [dimension](const Point& a, const Point& b){
-												   	   return a.coordinates[dimension] > b.coordinates[dimension];
+												   	   return a.getCoordinate(dimension) < b.getCoordinate(dimension);
 												   });
 			
 			Point p = *(begin + (size/2));
-			std::cout<<"[insert] new node, point=("<<p.coordinates[0]<<", "<<p.coordinates[1]<<")"<<std::endl; 
+			std::cout<<"[insert] new node split "<<dimension<<" dimension, point="<<p.toString()<<std::endl;
+
+			//std::cout<<"based on: "<<std::endl;
+			//for(auto i = begin; i != end; i++){
+			//	std::cout<<(*i).toString()<<std::endl;
+			//}
 
 			node = new Node(p, dimension);
-
+			
 			insert(node->left, begin, (begin + (size/2)), (dimension+1)%(this->dimensions));
 			insert(node->right, (begin + (size/2))+1, end, (dimension+1)%(this->dimensions));
 		}
--- kd-tree/test.cpp
+++ kd-tree/test.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <vector>
 #include <random>
 #include "kd-tree.hpp"
 
-
-const std::size_t numPoints = 5;
+const std::size_t numPoints = 10;
 const std::size_t dimensions = 2;
 const double coordinateMin = 0;
-const double coordinateMax = 10;
+const double coordinateMax = 15;
+
+bool generate_points(std::vector<bigno::Point>& points);
+bool import_points(const std::string fileName, std::vector<bigno::Point>& points);
+bool export_points(const std::string fileName, const std::vector<bigno::Point>& points);
 
 int main(){
-
 	
 	std::vector<bigno::Point> points;
+
+	//generate_points(points);
+	//export_points("p1.txt", points);
+	
+	import_points("p1.txt", points);
+
+	std::cout<<"points:"<<std::endl;
+	for(bigno::Point p : points){
+		std::cout<<p.toString()<<std::endl;
+	}
+
+	bigno::KdTree tree(dimensions, points);
+
+	std::vector<double> p1 = {3.5, 12};
+	tree.query_neighbors(bigno::Point(p1, new std::string("START")), 1);
+
+	return 0;
+}
+
+bool generate_points(std::vector<bigno::Point>& points){
 
 	for(std::size_t i=0; i<numPoints; i++){
 		
@@ -24,21 +48,70 @@ int main(){
 			coordinates.push_back(dist(rnd));
 		}
 
-		bigno::Point p(coordinates);
+		bigno::Point p(coordinates, new std::string(1, 'A'+i));
 		points.push_back(p);
 	}
 
-	std::cout<<"points:"<<std::endl;
-	for(bigno::Point p : points){
-		std::cout<<"(";
-		for(std::size_t i=0; i<dimensions; i++){
-			std::cout<<p.coordinates[i]<<", ";
-		}
-		std::cout<<")"<<std::endl;
+	return true;
+}
+
+bool import_points(const std::string fileName, std::vector<bigno::Point>& points){
+
+	std::ifstream infile(fileName);
+	if(!infile.is_open()){
+		return false;
 	}
 
-	bigno::KdTree tree(dimensions, points);
+	std::string line;
+	while(std::getline(infile, line)){
+		std::stringstream ss(line);
 
+		std::vector<double> coordinates;
+		double coordinate;
 
-	return 0;
+		std::string data;
+		bool dataFlag = true;
+		
+		while(std::getline(ss, line, ',')){
+			std::stringstream coordinateStream(line);
+			
+			if(dataFlag){
+				coordinateStream>>data;
+				dataFlag = false;
+			}else{
+				coordinateStream>>coordinate;
+				coordinates.push_back(coordinate);
+			}
+		}
+
+		if(!coordinates.empty()){
+			points.push_back(bigno::Point(coordinates, new std::string(data)));
+		}
+	}
+
+	infile.close();
+
+	return true;
+}
+
+bool export_points(const std::string fileName, const std::vector<bigno::Point>& points){
+
+	std::ofstream outfile(fileName);
+	if(!outfile.is_open()){
+		return false;
+	}
+
+	for(const bigno::Point& point : points){
+		outfile<<point.getDataString()<<',';
+		for(std::size_t i=0; i<point.getDimensions(); i++){
+			outfile<<point.getCoordinate(i);
+			if(i < point.getDimensions()-1)
+				outfile<<',';
+		}
+		outfile<<std::endl;
+	}
+
+	outfile.close();
+
+	return true;
 }
