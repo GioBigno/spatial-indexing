0d46ecb6b2f68f2da9a28fe795cb844006f37ef0
giorgio bignotti <giorgio.bigno@gmail.com>
2024-05-19 00:00:54 +0200

    using utils classes

--- kd-tree/kd-tree.hpp
+++ kd-tree/kd-tree.hpp
@@ -1,84 +1,14 @@
 #include <iostream>
 #include <utility>
 #include <vector>
-#include <limits>
 #include <cmath>
 #include <algorithm>
 #include <memory>
+#include "../utils/point.hpp"
+
+//#define DEBUG_TREE
 
 namespace bigno{
-
-class Point{
-
-	public:
-		Point(){
-			this->coordinates = {};
-		}
-		
-		Point(std::vector<double> v, void* data = nullptr){
-			this->coordinates = v;
-			this->setData(data);
-		}
-
-		std::size_t getDimensions() const {
-			return this->coordinates.size();
-		}
-
-		double getCoordinate(const std::size_t dimension) const {
-			return this->coordinates[dimension];
-		}
-
-		void* getData() const {
-			return this->data;
-		}
-
-		std::string getDataString() const {
-			return this->data == nullptr ? "" : *(static_cast<std::string*>(this->getData()));
-		}
-
-		void setData(void* data){
-			this->data = data;
-		}
-
-		int cmp(const Point& target, const std::size_t dimension) const {
-			double ret = this->coordinates[dimension] - target.coordinates[dimension];
-
-			if(fabs(ret) < std::numeric_limits<double>::epsilon()){
-				return 0;
-			}else{
-				return ret < 0 ? -1 : 1;
-			}
-		}
-
-		double distance(const Point& p) const {
-			
-			std::size_t dimensions = std::min(this->coordinates.size(), p.coordinates.size());
-			double sum = 0;
-
-			for(std::size_t i=0; i<dimensions; i++){
-				sum += std::pow(this->coordinates[i] - p.coordinates[i], 2);
-			}
-
-			return std::sqrt(sum);
-		}
-
-		std::string toString() const {
-			std::string ret = this->getDataString();
-			ret += "(";
-			for(std::size_t i=0; i<this->coordinates.size(); i++){
-				ret += std::to_string(this->coordinates[i]);
-				if(i != this->coordinates.size()-1)
-					ret += ", ";
-			}
-			ret += ")";
-			return ret;
-		}
-
-	private:
-
-		std::vector<double> coordinates;
-		void* data = nullptr;
-};
 
 class KdTree{
 
@@ -93,7 +23,6 @@ class KdTree{
 			Point a;
 			return this->query(target, this->root, {-1, a}).second;
 		}
-		
 
 	private:
 
