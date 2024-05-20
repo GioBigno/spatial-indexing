cc76391702ca5a9145ffd49fb507e5eb90f6e85b
giorgio bignotti <giorgio.bigno@gmail.com>
2024-05-20 22:36:20 +0200

    quad-tree

--- quad-tree/quad-tree.hpp
+++ quad-tree/quad-tree.hpp
@@ -3,6 +3,8 @@
 #include "../utils/point.hpp"
 #include "../utils/rect.hpp"
 
+#include <iostream>
+
 namespace bigno{
 
 class QuadTree{
@@ -11,24 +13,22 @@ class QuadTree{
 	public:
 
 		QuadTree(const Rect area, std::size_t maxPoints=3){
-			this->root = new Node(area);
+			this->root = std::make_unique<Node>(area);
 			this->MAX_POINTS = maxPoints;
 		}
 
-		bool insert(Point &p){
+		bool insert(const Point &p){
 			
-			Node* currentNode = nullptr;
 			
-			//check the root is exist and the point is in the tree's area
-			if(root != nullptr && root->area.contains(p)){
-				currentNode = root;
-			}else{
+			if(this->root.get() == nullptr){
 				return false;
 			}
 
-			//TODO
-			
-			return false;
+			return this->insert(this->root, p);
+		}
+
+		void debug(){
+			this->debug(this->root);
 		}
 
 	private:
@@ -39,7 +39,7 @@ class QuadTree{
 			std::vector<std::unique_ptr<Node>> childNodes;
 			std::vector<Point> points;
 		
-			Node(const Rect &area){
+			Node(const Rect& area){
 				this->area = area;
 			}
 
@@ -48,8 +48,74 @@ class QuadTree{
 			}
 		};
 
+		void debug(std::unique_ptr<Node>& node){
+
+			std::cout<<"node: "<< node.get()->area.getBottomLeft().toString() << ", " << node.get()->area.getTopRight().toString() <<std::endl;
+			std::cout<<"points: "<<std::endl;
+			for(Point p : node.get()->points){
+				std::cout<<p.toString()<<" ";
+			}
+			std::cout<<std::endl;
+			std::cout<<"childs: "<<node.get()->childNodes.size()<<std::endl;
+
+			for(std::unique_ptr<Node>& n : node.get()->childNodes){
+				this->debug(n);
+			}
+		}
+
+		bool insert(std::unique_ptr<Node>& node, const Point& p){
+		
+			if(!node.get()->area.contains(p)){
+				return false;
+			}
+
+			if(node.get()->isLeaf()){
+			
+				if(node.get()->points.size() < this->MAX_POINTS){
+					node.get()->points.push_back(p);
+					return true;
+				}
+
+				Rect currArea = node.get()->area;
+				double leftX = currArea.getBottomLeft().getX();
+				double rightX = currArea.getTopRight().getX();
+				double topY = currArea.getTopRight().getY();
+				double bottomY = currArea.getBottomLeft().getY();
+				double middleX = (currArea.getBottomLeft().getX() + currArea.getTopRight().getX())/2.0;
+				double middleY = (currArea.getBottomLeft().getY() + currArea.getTopRight().getY())/2.0;
+
+				node.get()->childNodes.push_back(std::make_unique<Node>(Rect(Point({leftX, middleY}), Point({middleX, topY}))));
+				node.get()->childNodes.push_back(std::make_unique<Node>(Rect(Point({middleX, middleY}), Point({rightX, topY}))));
+				node.get()->childNodes.push_back(std::make_unique<Node>(Rect(Point({leftX, bottomY}), Point({middleX, middleY}))));
+				node.get()->childNodes.push_back(std::make_unique<Node>(Rect(Point({middleX, bottomY}), Point({rightX, middleY}))));
+			
+				for(std::unique_ptr<Node>& n : node.get()->childNodes){
+					this->insert(n, p);
+				}
+
+				for(const Point& oldPoint : node.get()->points){
+					for(std::unique_ptr<Node>& n : node.get()->childNodes){
+						this->insert(n, oldPoint);
+					}
+				}
+
+				node.get()->points.clear();
+				return true;
+
+			}else{
+				
+				for(std::unique_ptr<Node>& n : node.get()->childNodes){
+					if(n.get()->area.contains(p)){
+						return this->insert(n, p);
+					}
+				}
+
+				return false;
+			}
+		}
+
 		std::size_t MAX_POINTS;
-		Node* root = nullptr;
+		std::unique_ptr<Node> root = nullptr;
 
 };
 
---
+++ quad-tree/test.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <random>
+#include "quad-tree.hpp"
+
+const std::size_t numPoints = 12;
+const std::size_t dimensions = 2;
+const double coordinateMin = 0;
+const double coordinateMax = 15;
+const size_t nodeMaxPoints = 3;
+
+bool generate_points(std::vector<bigno::Point>& points);
+bool import_points(const std::string fileName, std::vector<bigno::Point>& points);
+bool export_points(const std::string fileName, const std::vector<bigno::Point>& points);
+
+int main(){
+	
+	std::vector<bigno::Point> points;
+
+	//generate_points(points);
+	//export_points("p1.txt", points);
+	
+	import_points("p1.txt", points);
+
+	std::cout<<"[POINTS]"<<std::endl;
+	for(bigno::Point p : points){
+		std::cout<<p.toString()<<std::endl;
+	}
+
+	//QUAD TREE
+
+	std::cout<<"[CREATION]"<<std::endl;
+	bigno::QuadTree tree(bigno::Rect(bigno::Point({coordinateMin, coordinateMin}), bigno::Point({coordinateMax, coordinateMax})), nodeMaxPoints);
+
+	for(bigno::Point p : points){
+		tree.insert(p);
+	}
+	
+	/*
+	bigno::Point p1({1, 9}, new std::string("START"));
+	
+	std::cout<<"[TREE QUERY]"<<std::endl;
+	std::cout<<"Point: "<<p1.toString()<<std::endl;
+	bigno::Point neighbor = tree.query_neighbor(p1);
+	std::cout<<"Neighbor: "<<neighbor.toString()<<std::endl;
+
+	//LINEAR SEARCH
+
+	std::cout<<"[LINEAR SEARCH]"<<std::endl;
+	for(bigno::Point p : points){
+		std::cout<<"distance from "<<p.toString()<<" = "<<p1.distance(p)<<std::endl;
+	}
+
+	*/
+
+	return 0;
+}
+
+bool generate_points(std::vector<bigno::Point>& points){
+
+	for(std::size_t i=0; i<numPoints; i++){
+		
+		std::vector<double> coordinates;
+
+		for(std::size_t j=0; j<dimensions; j++){
+			std::default_random_engine rnd{std::random_device{}()};
+			std::uniform_real_distribution<double> dist(coordinateMin, coordinateMax);
+			coordinates.push_back(dist(rnd));
+		}
+
+		std::string name = "P" + std::to_string(i);
+		bigno::Point p(coordinates, new std::string(name));
+		points.push_back(p);
+	}
+
+	return true;
+}
+
+bool import_points(const std::string fileName, std::vector<bigno::Point>& points){
+
+	std::ifstream infile(fileName);
+	if(!infile.is_open()){
+		return false;
+	}
+
+	std::string line;
+	while(std::getline(infile, line)){
+		std::stringstream ss(line);
+
+		std::vector<double> coordinates;
+		double coordinate;
+
+		std::string data;
+		bool dataFlag = true;
+		
+		while(std::getline(ss, line, ',')){
+			std::stringstream coordinateStream(line);
+			
+			if(dataFlag){
+				coordinateStream>>data;
+				dataFlag = false;
+			}else{
+				coordinateStream>>coordinate;
+				coordinates.push_back(coordinate);
+			}
+		}
+
+		if(!coordinates.empty()){
+			points.push_back(bigno::Point(coordinates, new std::string(data)));
+		}
+	}
+
+	infile.close();
+
+	return true;
+}
+
+bool export_points(const std::string fileName, const std::vector<bigno::Point>& points){
+
+	std::ofstream outfile(fileName);
+	if(!outfile.is_open()){
+		return false;
+	}
+
+	for(const bigno::Point& point : points){
+		outfile<<point.getDataString()<<',';
+		for(std::size_t i=0; i<point.getDimensions(); i++){
+			outfile<<point.getCoordinate(i);
+			if(i < point.getDimensions()-1)
+				outfile<<',';
+		}
+		outfile<<std::endl;
+	}
+
+	outfile.close();
+
+	return true;
+}
