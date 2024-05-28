5f8d5ab5f0d074a5397ec3fdf5cd959791adc451
giorgio bignotti <giorgio.bigno@gmail.com>
2024-05-28 23:07:47 +0200

    added build kd-tree command

--- main.cpp
+++ main.cpp
@@ -5,29 +5,30 @@
 #include <cli/filehistorystorage.h>
 #include <cli/loopscheduler.h>
 #include <iostream>
+#include <memory>
+#include "headers/point.hpp"
+#include "kd-tree/kd-tree.hpp"
+#include "quad-tree/quad-tree.hpp"
 #include "utils/headers/shpreader.h"
 
-void readShapeFile(const std::string& fileName, std::vector<std::unique_ptr<geos::geom::Geometry>>& geometries);
+std::vector<std::shared_ptr<geos::geom::Geometry>> geometries;
+
+std::unique_ptr<bigno::KdTree> kdTree = nullptr;
+std::unique_ptr<bigno::QuadTree> quadTree = nullptr;
+
+bool readShapeFile(const std::string& fileName, std::vector<std::shared_ptr<geos::geom::Geometry>>& geometries);
+
+void cmd_build(std::ostream& out, const std::string& type, const std::string& inputFile);
 
 int main() {
-
-	std::vector<std::unique_ptr<geos::geom::Geometry>> geometries;
 
 	auto rootMenu = std::make_unique<cli::Menu>("cli");
 
     rootMenu->Insert(
-        "add", {"op1", "op2"},
-        [](std::ostream& out, const int op1_, const int op2_){
-
-            //std::string str_out;
-            //std::ostringstream stream_out(str_out);
-            //login(out, username_, password_);
-
-			out<<op1_+op2_<<std::endl;
-
-            //out<<stream_out.str();
-        },
-        "somma di due numeri interi"
+        "build",
+		{"type", "input"},
+		[](std::ostream& out, const std::string& type, const std::string& inputFile){cmd_build(out, type, inputFile);},
+        "--type [kd-tree|quad-tree|r-tree] --input [file.shp]"
         );
 
 	cli::Cli cli( std::move(rootMenu), std::make_unique<cli::FileHistoryStorage>(".cli") );
@@ -46,7 +47,7 @@ int main() {
     localSession.ExitAction(
         [&scheduler](std::ostream& out)
         {
-            out << "Closing App...\n";
+            out << "bye\n";
             scheduler.Stop();
         }
         );
@@ -56,17 +57,78 @@ int main() {
     return 0;
 }
 
+bool buildKdTree(std::ostream& out){
 
-void readShapeFile(const std::string& fileName, std::vector<std::unique_ptr<geos::geom::Geometry>>& geometries){
+	std::vector<bigno::Point> points;
+	
+	for(const std::shared_ptr<geos::geom::Geometry>& geom : geometries){
+		if(geom->getGeometryTypeId() != geos::geom::GEOS_POINT){
+			out<<"Error: in kd-tree all geometries must be points"<<std::endl;
+			return false; 
+		}
+		std::shared_ptr<geos::geom::Point> point = std::static_pointer_cast<geos::geom::Point>(geom); 
+		bigno::Point p({point->getX(), point->getY()}, nullptr);
+		points.push_back(p);
+	}
+
+	kdTree = std::make_unique<bigno::KdTree>(2, points);
+
+	return true;
+}
+
+void cmd_build(std::ostream& out, const std::string& type, const std::string& inputFile){
+	
+	if(!readShapeFile(inputFile, geometries)){
+		return;
+	}
+
+	//build the data structure
+
+	double time = 0;
+
+
+	if(type == "kd-tree"){
+
+		const auto start = std::chrono::steady_clock::now();
+		bool buildSuccess = buildKdTree(out);
+		const auto end = std::chrono::steady_clock::now();
+		std::chrono::duration<double> elapsed = end - start;
+		time = elapsed.count();
+
+		if(!buildSuccess){
+			return;
+		}
+
+	}else if(type == "quad-tree"){
+		//TODO
+	}else if(type == "r-tree"){
+		//TODO
+	}else{
+		out<<"Error: Invalid data structure type '"<<type<<"'"<<std::endl;
+		return;
+	}
+
+	out<<type<<" built successfully"<<std::endl
+	<<"time: "<<time<<" seconds"<<std::endl
+	<<"points: "<<geometries.size()<<std::endl;
+
+}
+
+
+bool readShapeFile(const std::string& fileName, std::vector<std::shared_ptr<geos::geom::Geometry>>& geometries){
 
     geometries.clear();
 
     bpp::ShpReader reader;
-    std::string openError;
+	std::string openError;
     reader.setFile(fileName);
     bool retval = reader.open(true, false, openError);
 
-    while(retval && reader.next()){
+	if(!retval){
+		return false;
+	}
+
+    while(reader.next()){
 
         std::unique_ptr<geos::geom::Geometry> currentGeom(nullptr);
 
@@ -91,6 +153,7 @@ void readShapeFile(const std::string& fileName, std::vector<std::unique_ptr<geos
 
         if(currentGeom)
             geometries.push_back(std::move(currentGeom));
-
     }
+
+	return true;
 }
