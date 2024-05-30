a6236bbb6ae2369b5dea6a375c9cb03835acfa5f
giorgio bignotti <giorgio.bigno@gmail.com>
2024-05-30 22:46:48 +0200

    minor changes and start implementing cmd_search_range

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <geom/Coordinate.h>
+#include <geom/Envelope.h>
 #include <geos/geom/Point.h>
 #include <geos/geom/GeometryFactory.h>
 #include <geos/index/kdtree/KdTree.h>
@@ -8,6 +9,7 @@
 #include <cli/clilocalsession.h>
 #include <cli/filehistorystorage.h>
 #include <cli/loopscheduler.h>
+#include <index/kdtree/KdNode.h>
 #include <iostream>
 #include <memory>
 #include <limits>
@@ -20,6 +22,7 @@ std::unique_ptr<geos::index::quadtree::Quadtree> quadTree;
 std::unique_ptr<geos::index::strtree::STRtree> rTree;
 
 void cmd_build(std::ostream& out, const std::string& type, const std::string& inputFile);
+void cmd_search_range(std::ostream& out, const std::string& type, const double x1, const double y1, const double x2, const double y2);
 bool readShapeFile(const std::string& fileName, std::vector<std::shared_ptr<geos::geom::Geometry>>& geometries);
 
 int main() {
@@ -33,6 +36,13 @@ int main() {
         "--type [kd-tree|quad-tree|r-tree] --input [file.shp]"
         );
 
+    rootMenu->Insert(
+        "search_range",
+		{"type", "query", "params"},
+		[](std::ostream& out, const std::string& type, const double x1, const double y1, const double x2, const double y2){cmd_search_range(out, type, x1, y1, x2, y2);},
+        "--type [kd-tree|quad-tree|r-tree] --x1 --y1 --x2 --y2"
+        );
+	
 	cli::Cli cli( std::move(rootMenu), std::make_unique<cli::FileHistoryStorage>(".cli") );
     cli.StdExceptionHandler(
         [](std::ostream& out, const std::string& cmd, const std::exception& e)
@@ -59,86 +69,96 @@ int main() {
     return 0;
 }
 
-bool buildKdTree(std::ostream& out){
-
-	kdTree = std::make_unique<geos::index::kdtree::KdTree>(std::numeric_limits<double>::epsilon());
-
-	for(const std::shared_ptr<geos::geom::Geometry>& geom : geometries){
-		if(geom->getGeometryTypeId() != geos::geom::GEOS_POINT){
-			out<<"Error: in kd-tree all geometries must be points"<<std::endl;
-			return false; 
-		}
-		geos::geom::Coordinate coord(*std::static_pointer_cast<geos::geom::Point>(geom)->getCoordinate());
-		kdTree->insert(coord, geom.get());
-	}
-
-	return true;
-}
-
-void buildQuadTree(std::ostream& out){
-
-	quadTree = std::make_unique<geos::index::quadtree::Quadtree>();
-
-	for(const std::shared_ptr<geos::geom::Geometry>& geom : geometries){
-		quadTree->insert(geom->getEnvelopeInternal(), geom.get());
-	}
-}
-
-void buildRTree(std::ostream& out){
-
-	rTree = std::make_unique<geos::index::strtree::STRtree>();
-
-	for(const std::shared_ptr<geos::geom::Geometry>& geom : geometries){
-		rTree->insert(geom->getEnvelopeInternal(), geom.get());
-	}
-}
-
 void cmd_build(std::ostream& out, const std::string& type, const std::string& inputFile){
 	
 	if(!readShapeFile(inputFile, geometries)){
 		return;
 	}
 
-	//build the data structure
-
-	std::chrono::duration<double> duration;
+	std::chrono::duration<double, std::milli> duration;
+	const auto start = std::chrono::steady_clock::now();
 	
 	if(type == "kd-tree"){
+	
+		kdTree = std::make_unique<geos::index::kdtree::KdTree>(std::numeric_limits<double>::epsilon());
 
-		const auto start = std::chrono::steady_clock::now();
-		bool buildSuccess = buildKdTree(out);
-		const auto end = std::chrono::steady_clock::now();
-		duration = end - start;
-
-		if(!buildSuccess){
-			return;
+		for(const std::shared_ptr<geos::geom::Geometry>& geom : geometries){
+			if(geom->getGeometryTypeId() != geos::geom::GEOS_POINT){
+				out<<"Error: in kd-tree all geometries must be points"<<std::endl;
+				return; 
+			}
+			geos::geom::Coordinate coord(*std::static_pointer_cast<geos::geom::Point>(geom)->getCoordinate());
+			kdTree->insert(coord, geom.get());
 		}
 
 	}else if(type == "quad-tree"){
 		
-		const auto start = std::chrono::steady_clock::now();
-		buildQuadTree(out);
-		const auto end = std::chrono::steady_clock::now();
-		duration = end - start;
+		quadTree = std::make_unique<geos::index::quadtree::Quadtree>();
+
+		for(const std::shared_ptr<geos::geom::Geometry>& geom : geometries){
+			quadTree->insert(geom->getEnvelopeInternal(), geom.get());
+		}
 
 	}else if(type == "r-tree"){
-		
-		const auto start = std::chrono::steady_clock::now();
-		buildRTree(out);
-		const auto end = std::chrono::steady_clock::now();
-		duration = end - start;
+	
+		rTree = std::make_unique<geos::index::strtree::STRtree>();
+
+		for(const std::shared_ptr<geos::geom::Geometry>& geom : geometries){
+			rTree->insert(geom->getEnvelopeInternal(), geom.get());
+		}
 
 	}else{
 		out<<"Error: Invalid data structure type '"<<type<<"'"<<std::endl;
 		return;
 	}
 
+	const auto end = std::chrono::steady_clock::now();
+	duration = end - start;
+
 	out<<type<<" built successfully"<<std::endl
-	<<"time: "<<(duration.count()*1000)<<" milli-seconds"<<std::endl
+	<<"time: "<<(duration.count())<<" milli-seconds"<<std::endl
 	<<"geometries: "<<geometries.size()<<std::endl;
 
 }
 
+void cmd_search_range(std::ostream& out, const std::string& type, const double x1, const double y1, const double x2, const double y2){
+
+	geos::geom::Envelope envelope(x1, x2, y1, y2);
+	std::size_t numGeom = 0;
+
+	out<<"params:"<<std::endl;
+	
+	out<<std::endl;
+
+	std::chrono::duration<double, std::milli> duration;
+	const auto start = std::chrono::steady_clock::now();
+	
+	if(type == "kd-tree"){
+	
+		std::vector<geos::index::kdtree::KdNode*> result;
+		
+		kdTree->query(envelope, result);
+
+		numGeom = result.size();
+
+		for(geos::index::kdtree::KdNode* node : result){
+			node->getData();
+		}
+
+	}else if(type == "quad-tree"){
+		
+	}else if(type == "r-tree"){
+		
+	}else{
+		out<<"Error: Invalid data structure type '"<<type<<"'"<<std::endl;
+		return;
+	}
+
+	out<<type<<" built successfully"<<std::endl
+	<<"time: "<<(duration.count())<<" milli-seconds"<<std::endl
+	<<"geometries: "<<numGeom<<std::endl;
+
+}
 
 bool readShapeFile(const std::string& fileName, std::vector<std::shared_ptr<geos::geom::Geometry>>& geometries){
 
